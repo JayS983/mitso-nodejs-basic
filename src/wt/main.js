df987a0ca505be4a92372061d91247ddd5115ac4
JayS <75631785+JayS983@users.noreply.github.com>
2024-05-18 19:44:01 +0000

    main.js (wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+const { Worker, threadId } = require('worker_threads');
+const os = require('os');
+
+const numThreads = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+    const results = [];
+
+    for (let i = 0; i < numThreads; i++) {
+        const worker = new Worker('./worker.js');
+        const startNumber = 10 + i;
+
+        const promise = new Promise((resolve) => {
+            worker.on('message', (result) => {
+                results[threadId - 1] = result;
+                resolve();
+            });
+        });
+
+        worker.postMessage(startNumber);
+        promises.push(promise);
+    }
+
+    await Promise.all(promises);
+
+    console.log(results);
 };
 
 await performCalculations();
